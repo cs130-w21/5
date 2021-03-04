942158475c004ed7a6f10b6f960387af7d6ccefb
Sum Li <sumli@Sums-MacBook-Pro.local>
2021-03-03 20:51:16 -0800

    small fix on using app drop down

--- frontend/src/pages/SearchPage/index.jsx
+++ frontend/src/pages/SearchPage/index.jsx
@@ -3,9 +3,19 @@ import Frame from "../../components/Frame";
 import { useHistory } from "react-router-dom";
 import SearchBar from "../../components/SeachBar";
 import TutorList from "../../components/TutorList";
+import { useState } from "react";
 const SearchPage = ({ uid, userStore, matchedTutors }) => {
   const history = useHistory();
   const tutors = [];
+
+  // You can pass these to the app drop down as: <AppDropDown onSelect={dropDownHandler} selectedClass={selectedClass} setSelectedClass={setSelectedClass}></AppDropDown>
+  const [selectClass, setSelectClass] = useState([]);
+
+  const dropDownHandler = (pickClass) => {
+    // set the state to contain the selected class 
+    setSelectClass(pickClass);
+  }
+
   matchedTutors.map((tutorId) => tutors.push(userStore[tutorId]));
   return (
     <PageFrame onTitleClick={() => history.push("/profile/" + uid)}>
